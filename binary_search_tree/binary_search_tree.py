1d9abd6ed89b06d140b9ae7aa79efbceac155f70
oops <oops@localhost.localdomain>
2020-03-26 18:46:29 -0400

    6.3.4 tests passed

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,8 @@
-# import sys
-# sys.path.append('../queue_and_stack')
-# from dll_queue import Queue
-# from dll_stack import Stack
+import sys
+
+sys.path.append("../queue_and_stack")
+from dll_queue import Queue
+from dll_stack import Stack
 
 
 class BinarySearchTree:
@@ -22,18 +23,21 @@ class BinarySearchTree:
             # if self.left is none: there no branch there
             # so make a new branch
             if self.left == None:
+                # if there is no branch: add a new one
                 self.left = BinarySearchTree(value_input)
 
             # if there is a branch keep going
             else:
                 self.left.insert(value_input)
 
-        if value_input >= self.value:
+        # yes: maybe thie should/could be 'else'?
+        elif value_input >= self.value:
             # go right (higher)
 
             # if self.left is none: there no branch there
             # so make a new branch
             if self.right == None:
+                # if there is no branch: add a new one
                 self.right = BinarySearchTree(value_input)
 
             # if there is a branch keep going
@@ -62,7 +66,7 @@ class BinarySearchTree:
                     return self.left.contains(target)
 
             # if biggger
-            if target >= self.value:
+            elif target >= self.value:
                 # if branch ends: output false, not found
                 if self.right == None:
                     return False
@@ -86,17 +90,7 @@ class BinarySearchTree:
     # for_each performs a traversal of every node in the tree,
     # executing the passed-in callback function on each tree node value.
     #
-    # There is myriad ways to perform tree traversal;
-    # in this case any of them should work.
-    #
-    # depth and breadth
-    # Call the function `cb` on the value of each node
-    # You may use a *recursive or iterative approach
-    #
-    # for_each performs a traversal of every node in the tree,
-    # executing the passed-in callback function on each tree node value.
-    #
-    # depth and breadth
+    # (depth and breadth on day 2)
     def for_each(self, cb):
         # # if the root is empty
         # if self.value == None:
@@ -125,20 +119,123 @@ class BinarySearchTree:
 
     # # DAY 2 Project -----------------------
 
-    # # Print all the values in order from low to high
-    # # Hint:  Use a recursive, depth first traversal
-    # def in_order_print(self, node):
-    #     pass
+    def print_all(self):
+        return self.for_each(print)
 
-    # # Print the value of every node, starting with the given node,
-    # # in an iterative breadth first traversal
-    # def bft_print(self, node):
-    #     pass
+    # Print all the values in order from low to high
+    # Hint:  Use a recursive, depth first traversal
+    def in_order_print(self, node):
 
-    # # Print the value of every node, starting with the given node,
-    # # in an iterative depth first traversal
-    # def dft_print(self, node):
-    #     pass
+        if node == None:
+            return None
+        self.in_order_print(node.left)
+        # print that lowest left
+        print(node.value)
+        self.in_order_print(node.right)
+
+        # if node == None:
+        #     return None
+        # self.in_order_print(node.left)
+        # # print that lowest left
+        # print(node.value)
+        # self.in_order_print(node.right)
+
+        # # look left
+        # if self.left != None:
+        #     # if left brach exists: go left
+        #     self.in_order_print(self.left)
+        # # print that lowest left
+        # print(self.value)
+        # # if no left, look right
+        # if self.right != None:
+        #     # if right exists, go right
+        #     self.in_order_print(self.right)
+        # # then repeat
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative
+    # breadth first traversal
+    #
+    # Level by level
+    def bft_print(self, node):
+        # make quququ
+        # add root
+        # while not empty
+        # print
+        # add children of node to queue
+
+        # # create a queue
+        q = Queue()
+        # enqueue firts item
+        q.enqueue(node)
+
+        # while not empty
+        while q.len() > 0:
+            temp = q.dequeue()
+            print(temp.value)
+            if temp.left:
+                q.enqueue(temp.left)
+            if temp.right:
+                q.enqueue(temp.right)
+
+        # print
+        # add children of node to queue
+
+        # print(self.value)
+
+        # if self.left:
+        #   self.bft_print(self.left)
+
+        # if self.right:
+        #   self.bft_print(self.left)
+
+        # # print the first node (root)
+        # print(self.value)
+
+        # while True:
+        #     # if right branch exists
+        #     if self.right != None:
+        #         # print its value
+        #         print(self.right.value)
+
+        #     # if right branch exists
+        #     if self.left != None:
+        #         # print its value
+        #         print(self.left.value)
+        #     break
+
+    # Print the value of every node, starting with the given node,
+    # in an iterative
+    # depth first traversal
+    def dft_print(self, node):
+
+        # not recursive? or iterative?
+        # Step 1: make a stack
+        # Step 2: add parent to stack
+        #
+        # while: Repeat steps 3 and 4
+        # Step 3: remove that parent from stack
+        #         print value when removed
+        # Step 4: add that parent's children to the stack
+        #         go left to right by convention
+        #
+
+        # Step 1: make a stack
+        dft_stack = Stack()
+        # Step 2: add parent to stack
+        dft_stack.push(node)
+
+        while dft_stack.len() > 0:
+            # Step 3: remove that parent from stack
+            temp = dft_stack.pop()
+            print(temp.value)
+            # Step 4: add that parent's children to the stack
+            #         go left to right by convention
+            if temp.left:
+                dft_stack.push(temp.left)
+            if temp.right:
+                dft_stack.push(temp.right)
+            # Repeat steps 3 and 4
 
     # # STRETCH Goals -------------------------
     # # Note: Research may be required
